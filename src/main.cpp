1dccf654e712e14cd330527fab4a3e5aad624270
David Cardozo <gd.cardozo684@uniandes.edu.co>
2019-04-21 20:51:00 -0500

    feat: Init Arch

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,12 @@ int main() {
   /**
    * TODO: Initialize the pid variable.
    */
+  double Kp_init = 0.15;
+  double Ki_init = 0.0005;
+  double Kd_init = 4.3;
 
+  PID pid;
+  pid.Init(Kp_init, Ki_init, Kd_init);
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -58,11 +63,13 @@ int main() {
           double angle = std::stod(j[1]["steering_angle"].get<string>());
           double steer_value;
           /**
-           * TODO: Calculate steering value here, remember the steering value is
+           * Calculate steering value here, remember the steering value is
            *   [-1, 1].
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
